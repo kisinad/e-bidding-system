330dc6342626b6480e78a4feca59e3eb1f8f78d7
Denis Kisina <kisinad@gmail.com>
2019-07-03 09:06:21 +0300

    registration&login work

---
+++ js/login.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+$('.message a').click(function(){
+$('form').animate({height:"toggle", opacity:"toggle"}, "slow");
+});
+
+// const companyname = $('#companyname');
+// const companyemail = $('#companyemail');
+// const companypassword = $('#companypassword');
+const create = $('#create');
+
+$('#create').click(function(){
+	const companyname = $('#companyname').val();
+	const companyemail = $('#companyemail').val();
+	const companypassword = $('#companypassword').val();
+	// console.log(companyname);
+	// console.log(companyemail);
+	// console.log(companypassword);
+
+	if (companyname && companyemail && companypassword){
+		localStorage.setItem("companyname", companyname);
+		localStorage.setItem("companyemail", companyemail);
+		localStorage.setItem("companypassword",companypassword);
+		location.reload();
+	}
+});
+$('#login').click(function(){
+	var inputEmail = $('#email').val();
+	var inputPassword = $('#password').val();
+	if((inputEmail == localStorage.getItem('companyemail')) && (inputPassword ==localStorage.getItem('companypassword'))){
+		// console.log("login sucessful!");
+		window.location.href="https://www.w3schools.com";
+	}
+	// const keys = Object.keys(localStorage)
+	// for (i=0; i<localStorage.length; i++){
+	// 	if()
+	// }
+});
+});
