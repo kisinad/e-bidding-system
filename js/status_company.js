95dfbf54a67bed742be4f19eb91d6260085436b4
Denis Ahimbisibwe <ahimbisibwedenis1982@gmail.com>
2019-07-04 11:53:19 +0300

    Updated files

--- js/status_company.js
+++ js/status_company.js
@@ -1,1 +1,25 @@
-$(document).ready(function(){}
+$(document).ready(function()
+{
+	
+$('#submit').click(function()
+{
+    const referenceNumber = $('#referenceNumber').val();
+    localStorage.setItem("ReferenceNumber", referenceNumber);
+
+    const companyName = $('#companyName').val();
+    localStorage.setItem("CompanyName",companyName);
+
+    const description = $('#description').val();
+    localStorage.setItem("Description", description);
+    
+    // {
+    // const no = $('#no').val();
+    // localStorage.setItem("no", no);
+    // const referencenumber = $('#name_of_Receipient').val();
+    // localStorage.setItem("name_of_Receipient",name_of_Receipient);
+    // const address = $('#address').val();
+    // localStorage.setItem("address", address);
+    // const addenda = $('#Addenda').val();
+    // localStorage.setItem("addenda", addenda);
+
+});
