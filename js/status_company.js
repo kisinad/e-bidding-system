971797a5a0132709a9b6a3ea8360d4b14685cabb
DMayanja21 <you@example.com>
2019-07-04 11:30:12 +0300

    status edits

--- js/status_company.js
+++ js/status_company.js
@@ -1,1 +1,21 @@
-$(document).ready(function(){}
+$(document).ready(function(){
+	$('#newTender').evt.preventDefault();
+
+	$('#reset').click(function clear(){
+		$('#ref').val()='';
+		$('#c_name').val()='';
+		$('#desc').val()='';
+	});
+
+	$('#save').click(function(){
+	const referenceNumber = $('#ref').val();
+	const companyName = $('#c_name').val();
+	const description = $('#desc').val();
+
+	if (referenceNumber && companyName && description){
+		localStorage.setItem("referenceNumber", referenceNumber);
+		localStorage.setItem("companyName", companyName);
+		localStorage.setItem("description",description);
+		location.reload();
+	};
+})	
