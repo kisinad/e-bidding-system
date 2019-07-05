4fdfb43aa7d328a0bce5bfde6f83e5641dcaf2bf
Denis Kisina <kisinad@gmail.com>
2019-07-05 08:33:23 +0300

    edited js files

--- js/status_company.js
+++ js/status_company.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
 	
-$('#submit').click(function clear()
+// $('#submit').click(function clear()
 
 
 //$('#newTender').evt.preventDefault();
@@ -10,20 +10,47 @@ $('#reset').click(function()
 {
     $('#referencenumber').val()='';
     $('#companyName').val()='';
-    $('#description').val()='';
+    $('#description').val()='';});
 
 $('#save').click(function()
 {
-    var referenceNumber = $('#referenceNumber').val();
-    localStorage.setItem("ReferenceNumber", referenceNumber);
+    const referenceNumber = $('#referenceNumber').val();
     const companyName = $('#companyName').val();
-    localStorage.setItem("CompanyName",companyName);s
     const description = $('#description').val();
-    var companyName = $('#companyName').val();
-    localStorage.setItem("CompanyName",companyName);
-    var description = $('#description').val();
-    localStorage.setItem("Description", description);
-    
+    const bidOutPut = $('#bidOutPut').val();
+    // var companyName = $('#companyName').val();
+    // var description = $('#description').val();
+
+    // localStorage.setItem("Description", description);
+    // localStorage.setItem("ReferenceNumber", referenceNumber);
+    // localStorage.setItem("CompanyName",companyName);
+   
+
+    // console.log(referenceNumber);
+    // console.log(companyName);
+    // console.log(description);
+    if (referenceNumber && companyName && description){
+        localStorage.setItem('referenceNumber', referenceNumber);
+        // console.log(localStorage.setItem(referenceNumber, companyName, description));
+        localStorage.setItem('description', description);
+        // localStorage.setItem("ReferenceNumber", referenceNumber);
+        localStorage.setItem("CompanyName",companyName);
+        // localStorage.setItem("CompanyName",companyName);
+       location.reload();
+       localStorage.getItem(referencenumber)
+        // console.log(referenceNumber);
+        // console.log(companyName);
+        // console.log(description);
+        $('div#bidOutPut').html(localStorage.key(0)) ;
+
+    }
+    // for (let i = 0; i<localStorage.length; i++)
+    //     var key = localStorage.key(i);
+    //     const value = localStorage.getItem(key);
+    //     console.log(value);
+        // $('#bidOutPut').html('key') ;
+        // $('#bidOutPut').html('key');
+        // $('#bidOutPut').append( "<p>Test</p>" );
     // {
     // const no = $('#no').val();
     // localStorage.setItem("no", no);
@@ -34,7 +61,7 @@ $('#save').click(function()
     // const addenda = $('#Addenda').val();
     // localStorage.setItem("addenda", addenda);
 });
-}):
+});
 // });
 // =======
 // $(document).ready(function(){
